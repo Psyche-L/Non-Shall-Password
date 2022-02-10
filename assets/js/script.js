93ab6943b2b280158673b5cc6b32ac7bc46b515a
Seveonno Palizzolo <homedome@Seveonnos-MacBook-Air.local>
2022-02-09 20:29:10 -0800

    confirm windows and available characters

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,30 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  var passwordLength = window.prompt(
+    "How many characters would you like to have? (Choose between 8 and 128)"
+
+  );
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    var passwordLower = window.confirm("Would you like lowercase letters?");
+    var passwordUpper = window.confirm("Would you like to uppercase letters?");
+    var passwordNumeric = window.confirm("Would you like to add numbers?");
+    var passwordSpecial = window.confirm("Would you like special characters?");
+    var fries = window.confirm("Would you like fries with that?");
+
+
+// Characters generater will use 
+    var emptyString = "";
+    var stringLower = "abcdefghijklmnopqrstuvwxyz";
+    var stringUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var stringNumeric = "0123456789"
+    var stringSpecial = "+_)(*&^%$#@!~`<>?:{}|"
+
+
+
+    
+  }
 
   passwordText.value = password;
 
